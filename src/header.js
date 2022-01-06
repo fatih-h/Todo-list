5e817d33ac3ae942b7a31ab3209f8e14b3ae5f19
Fatih H <fatih.hsu1907@gmail.com>
2022-01-06 21:45:31 +0300

    Header Section Done

---
+++ src/header.js
@@ -0,0 +1,15 @@
+import './styles/header.css';
+
+function header(){
+    let main = document.createElement('div');
+    main.classList.add('header-box');
+
+    let heading = document.createElement('h1');
+    heading.classList.add('header-heading');
+    
+    heading.textContent = 'To-Do List';
+
+    main.appendChild(heading);
+    return main;
+}
+export default header;
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import './styles/style.css';
+import header from './header';
+// import main from './main';
+// import sidebar from './sidebar';
+
+
+
+document.getElementById('content');
+
+content.appendChild(header());
