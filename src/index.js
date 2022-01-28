ba4788b4b7f2a2f932cc3103e39c8dfe404f44b2
Fatih H <fatih.hsu1907@gmail.com>
2022-01-28 20:06:54 +0300

    Project part deleted and turn back to make basic todo

--- src/index.js
+++ src/index.js
@@ -15,73 +15,51 @@ document.getElementById('content');
     content.appendChild(doItem());
 })();
 
-(function () {
+document.getElementsByClassName('plus-button')[0].addEventListener('click', () => {
     let mainBox = document.getElementsByClassName('main-box')[0];
-    let result = true;
-    document.getElementsByClassName('plus-button')[0].addEventListener('click', () => {
+    mainBox.appendChild(makeTodo('General'));
+});
 
-        if (result) {
-            mainBox.appendChild(makeTodo('General'));
-        } else {
-            let key = document.getElementsByClassName('side-project-new')[0].textContent.split(' ').join('');
-            mainBox.appendChild(makeTodo(key));
-            Array.from(document.getElementsByClassName('main-todo')).forEach(event => {
-                if (event.childNodes[4].classList[1] == key) {
-                    event.style.display = 'grid';
+
+document.getElementsByClassName('side-box')[0].addEventListener('click', (e) => {
+    if (e.target.type == 'button') {
+
+        if (e.target.classList[0] == 'side-button-home') {
+            Array.from(document.getElementsByClassName('main-todo')).forEach((event) => {
+                event.style.display = 'grid';
+            });
+        }
+
+        else if (e.target.classList[0] == 'side-button-week') {
+
+            Array.from(document.getElementsByClassName('main-todo-dueDate')).forEach((event) => {
+                let counter = 0;
+                for (let i = 0; i < 7; i++) {
+                    let x = add(new Date(), { days: i });
+                    if (event.value == format(x, 'y-MM-dd')) {
+                        counter++;
+                    }
+                }
+                if (counter > 0) {
+                    event.parentElement.style.display = 'grid';
+                } else {
+                    event.parentElement.style.display = 'none';
+                }
+
+            });
+        }
+
+        else if (e.target.classList[0] == 'side-button-today') {
+
+            Array.from(document.getElementsByClassName('main-todo-dueDate')).forEach((event) => {
+                if (event.value == format(new Date(), 'y-MM-dd')) {
+                    event.parentElement.style.display = 'grid';
                 }
                 else {
-                    event.style.display = 'none';
+                    event.parentElement.style.display = 'none';
                 }
             });
         }
-    });
+    }
 
-
-    document.getElementsByClassName('side-box')[0].addEventListener('click', (e) => {
-        if (e.target.type == 'button') {
-
-            if (e.target.classList[0] == 'side-button-home') {
-                result = true;
-                Array.from(document.getElementsByClassName('main-todo')).forEach((event) => {
-                    event.style.display = 'grid';
-                });
-            }
-
-            else if (e.target.classList[0] == 'side-button-week') {
-                result = true;
-                Array.from(document.getElementsByClassName('main-todo-dueDate')).forEach((event) => {
-                    let counter = 0;
-                    for (let i = 0; i < 7; i++) {
-                        let x = add(new Date(), { days: i });
-                        if (event.value == format(x, 'y-MM-dd')) {
-                            counter++;
-                        }
-                    }
-                    if (counter > 0) {
-                        event.parentElement.style.display = 'grid';
-                    } else {
-                        event.parentElement.style.display = 'none';
-                    }
-
-                });
-            }
-
-            else if (e.target.classList[0] == 'side-button-today') {
-                result = true;
-                Array.from(document.getElementsByClassName('main-todo-dueDate')).forEach((event) => {
-                    if (event.value == format(new Date(), 'y-MM-dd')) {
-                        event.parentElement.style.display = 'grid';
-                    }
-                    else {
-                        event.parentElement.style.display = 'none';
-                    }
-                });
-            }
-            else if (e.target.classList[0] == 'side-project-new') {
-                result = false;
-            }
-        }
-
-    });
-
-})();
+});
--- src/sidebar.js
+++ src/sidebar.js
@@ -12,55 +12,52 @@ function sidebar() {
     let week = document.createElement('button');
     let today = document.createElement('button');
     let project = document.createElement('div');
-    let heading = document.createElement('h2');
-    let add = document.createElement('button');
+    // let heading = document.createElement('h2');
+    // let add = document.createElement('button');
 
     navbar.classList.add('side-button');
     home.classList.add('side-button-home');
     today.classList.add('side-button-today');
     week.classList.add('side-button-week');
     project.classList.add('side-project')
-    heading.classList.add('side-project-heading');
-    add.classList.add('side-project-add');
+    // heading.classList.add('side-project-heading');
+    // add.classList.add('side-project-add');
 
     home.setAttribute('type', 'button');
     week.setAttribute('type', 'button');
     today.setAttribute('type', 'button');
-    add.setAttribute('type', 'button');
+    // add.setAttribute('type', 'button');
 
     home.textContent = 'Home';
     week.textContent = 'Week';
     today.textContent = 'Today';
-    heading.textContent = 'Projects';
-    add.textContent = 'Add New Project';
-    let i = 0;
-    add.addEventListener('click', (event) => {
-        let text = document.createElement('input');
-        text.setAttribute('type', 'text');
-        add.before(text);
-        text.addEventListener("keyup", (e) => {
-            if (e.key == "Enter") {
-                let nButton = document.createElement('button');
-                nButton.setAttribute('type', 'button');
-                nButton.classList.add('side-project-new');
-                i++;
-                nButton.textContent = text.value;
+    // heading.textContent = 'Projects';
+    // add.textContent = 'Add New Project';
+    // add.addEventListener('click', (event) => {
+    //     let text = document.createElement('input');
+    //     text.setAttribute('type', 'text');
+    //     add.before(text);
+    //     text.addEventListener("keyup", (e) => {
+    //         if (e.key == "Enter") {
+    //             let nButton = document.createElement('button');
+    //             nButton.setAttribute('type', 'button');
+    //             nButton.classList.add('side-project-new');
+    //             nButton.textContent = text.value;
+    //             text.after(nButton);
+    //             text.remove();
 
-                text.after(nButton);
-                text.remove();
+    //         }
+    //     })
 
-            }
-        })
-
-    });
+    // });
 
     main.appendChild(navbar);
     main.appendChild(project);
     navbar.appendChild(home);
     navbar.appendChild(week);
     navbar.appendChild(today);
-    project.appendChild(heading);
-    project.appendChild(add);
+    // project.appendChild(heading);
+    // project.appendChild(add);
 
 
     return main;
